c577a22bc4be21ea3e1ea326e9aaf90210862acc
Eyouel Getachew <eyouelgk@gmail.com>
2025-05-26 23:36:17 +0300

    removed neon again :)

--- db/index.ts
+++ db/index.ts
@@ -1,16 +1,9 @@
-import { drizzle as drizzlePostgres } from "drizzle-orm/node-postgres"
-
+// db/index.ts for supabase
+import { drizzle } from "drizzle-orm/postgres-js"
+import postgres from "postgres"
 import * as schema from "./schema"
 
-export const db = process.env.VERCEL
-drizzlePostgres(process.env.DATABASE_URL!, { schema, casing: "snake_case" })
+const connectionString = process.env.DATABASE_URL!
 
-// db/index.ts for supabase
-// import { drizzle } from "drizzle-orm/postgres-js"
-// import postgres from "postgres"
-// import * as schema from "./schema"
-
-// const connectionString = process.env.DATABASE_URL!
-
-// const client = postgres(connectionString, { ssl: "allow", prepare: false })
-// export const db = drizzle(client, { schema, casing: "snake_case" })
+const client = postgres(connectionString, { ssl: "allow", prepare: false })
+export const db = drizzle(client, { schema, casing: "snake_case" })
