a4cb8cfee9bbcce73884c7a5f651ace133865b1e
Eyouel Getachew <eyouelgk@gmail.com>
2025-05-26 23:33:25 +0300

    removed neondb

--- db/index.ts
+++ db/index.ts
@@ -1,16 +1,9 @@
-import { drizzle as drizzleNeon } from "drizzle-orm/neon-http"
 import { drizzle as drizzlePostgres } from "drizzle-orm/node-postgres"
-import { neon } from "@neondatabase/serverless"
 
 import * as schema from "./schema"
 
 export const db = process.env.VERCEL
-  ? drizzleNeon({
-      client: neon(process.env.DATABASE_URL!),
-      schema,
-      casing: "snake_case",
-    })
-  : drizzlePostgres(process.env.DATABASE_URL!, { schema, casing: "snake_case" })
+drizzlePostgres(process.env.DATABASE_URL!, { schema, casing: "snake_case" })
 
 // db/index.ts for supabase
 // import { drizzle } from "drizzle-orm/postgres-js"
